4750d23970b503ce94b6a7f8017a9f70d3577707
Adrian Hecker <starcalibre@gmail.com>
2016-03-06 12:05:16 +1100

    Add test configuraton class * Points Mongo to test database

--- config.py
+++ config.py
@@ -18,18 +18,21 @@ class Config:
     # corresponds to the "size" property in the HTML tag <select>
     UI_GENELIST_HEIGHT = 7
 
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-    MINIFY_HTML = False
-
     # sessions which are used for managing flash messages. a secret
     # key must be declared in order to use sessions.
     SECRET_KEY = os.environ.get("MICROSCOPIUM_SECRET_KEY") or "secret string"
 
+
+class DevelopmentConfig(Config):
+    MINIFY_HTML = False
     MONGO_DBNAME = "microscopium"
+
+
+class TestConfig(Config):
+    MONGO_DBNAME = "microscopium-test"
 
 config = {
     "development": DevelopmentConfig,
-    "default": DevelopmentConfig
+    "default": DevelopmentConfig,
+    "test": TestConfig
 }
